40c0f45ac7a6a3c88b3293a660626600a3442c6a
Jafagervik <jorgenafagervik@gmail.com>
2021-10-05 17:25:56 +0200

    vis

--- include/filehandling.hpp
+++ include/filehandling.hpp
@@ -4,6 +4,7 @@
 #include <string_view>
 #include <vector>
 #include <stdint.h>
+#include <unordered_map>
 
 std::vector<std::string> read_file(const std::string &filename);
-void write_file(const std::string &filename);
+void write_file(const std::string &filename, std::vector<uint16_t> seqs);
--- visualize.py
+++ visualize.py
@@ -2,6 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def read_distribution(filename: str):
+    """Read a distribution from a file
+    inp: >>> 1 4\n2 5\0
+    out: >>> dict{"1": 4, "2": 5}
+    """
+    with open(filename, "r") as f:
+        f.read
+
+    return
+
+
 def main():
     print("----- E N T R Y P O I N T -----")
 
