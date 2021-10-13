413d63c3582b02d339f4d4eb0fbf921c363d8ac0
Jafagervik <jorgenafagervik@gmail.com>
2021-10-14 01:33:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,17 @@
+import logging
 import numba
 from numba import njit
 import numpy as np
 import matplotlib.pyplot as plt
 
-from .visualize import *
-
 A = "TGGAATTCTCGGGTGCCAAGGAACTCCAGTCACACAGTGATCTCGTATGCCGTCTTCTGCTTG"
 
 
 @njit(parallel=True, fastmath=True)
-def knut_morris_plot(T: str, P: str) -> tuple(list[int], int):
-    """Substring search algorithm O(m+n)
-
-    TODO: find way to implement mismatches
-    """
-
-    # longest prefix sufffix
-
-    m = len(T)
-    n = len(P)
-    lps = [0] * n
-    j = 0
-
-    compute_lcs_array(P, n, lps)
-    i = 0
-    while i < n:
-        if P[j] == T[i]:
-            i += 1
-            j += 1
-            # If we're at end
-            if j == n:
-                if (i - j) == 0:
-                    print(f"Found pattern at index {str(i-j)}")
-                    return True
-                j = lps[j - 1]
-
-        elif i < m and P[j] != T[i]:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                i += 1
-    return False
-
-
-@njit(parallel=True, fastmath=True)
-def compute_lcs_array(P: str, n: int, lps: list[int]):
-    # length of the previous longest prefix suffix
-    l = 0
-    lps[0] = 0
-    i = 1
-
-    while i < n:
-        if P[i] == P[l]:
-            l += 1
-            lps[i] = l
-            i += 1
-        else:
-            if l:
-                l = lps[l - 1]
-            else:
-                lps[i] = 0
-                i += 1
-
-
-@njit(parallel=True, fastmath=True)
-def string_compare(A: str, B: str, mismatch_percentage: float):
+def string_compare(
+    A: str, B: str, mismatch_percentage: float, allow_ins_del: bool = False
+):
+    # TODO: implement insertion deletion
     if A == B:
         return True
 
@@ -81,22 +28,25 @@ def string_compare(A: str, B: str, mismatch_percentage: float):
     return same_char_at_index / max_len >= (1 - mismatch_percentage)
 
 
+# @njit()
 def read_seq3(filename: str):
-    with open(filename, "r") as f:
-        yield from f.readlines()
+    for line in open(filename, "r"):
+        yield line
 
 
-@njit(parallel=True, fastmath=True)
+# @njit(parallel=True, fastmath=True)
 def mismatch(mismatch_percentage: float):
     num_of_sequences = 0
     allowed_seqs = np.array([])
 
-    gen = read_seq3(".\\data\\s_3_sequence_1M.txt")
+    gen = next(read_seq3(".\\data\\s_3_sequence_1M.txt"))
 
     # FIXME: Handle exception better
     while gen:
         for i in range(len(gen)):
-            suffix_prefix = string_compare(A[: len(gen) - i], gen[i:])
+            suffix_prefix = string_compare(
+                A[: len(gen) - i], gen[i:], mismatch_percentage
+            )
             if suffix_prefix:
                 num_of_sequences += 1
                 allowed_seqs.append(gen)
@@ -108,13 +58,21 @@ def mismatch(mismatch_percentage: float):
     return allowed_seqs
 
 
+@njit(fastmath=True)
+def make_bins(size: int):
+    return np.array([i for i in range(size + 1)])
+
+
 def make_distribution(array):
     max_size = np.amax(array)
-    bins = np.array([i for i in range(max_size + 1)])
+    bins = make_bins(max_size)
     plt.hist(array, bins)
     plt.show()
 
 
+if __name__ == "__main__":
+    case1 = mismatch(0.0)
+    make_distribution(case1)
 ## TEST
 """
 mismatch(0.0)
