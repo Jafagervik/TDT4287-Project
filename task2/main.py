a227e2d33ffdde06e25cbfc3132278e1a5f3050f
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2021-10-24 17:07:16 +0200

    just need to allow ins/dels now and task 2 is golden

--- task2/main.py
+++ task2/main.py
@@ -10,6 +10,9 @@ from helpers import read_file, A
 def string_compare(
     A: str, B: str, mismatch_percentage: float, allow_ins_del: bool = False
 ):
+    """
+    Hamming distance between two strings.
+    """
     # TODO: implement insertion deletion
     if A == B:
         return True
@@ -28,12 +31,14 @@ def string_compare(
     return a >= (1.0 - mismatch_percentage)
 
 
-def mismatch(content, mismatch_percentage: float):
+def mismatch(
+    content, mismatch_percentage: float, allow_ins_del: bool = False
+) -> list[str]:
     allowed_seqs = []
 
     # FIXME: Handle exception better
     for line in tqdm(content):
-        line = line.rstrip()
+        line = line.rstrip()  # Remove trailing newline
         l = len(line)
         for i in range(l):
             # Match prefix of a with suffix of s or "line" in this instance
@@ -54,7 +59,7 @@ def make_bins(size: int):
     return np.array([i for i in range(size + 1)])
 
 
-def make_distribution(array):
+def make_distribution(array, filename: str, show: bool = False):
     try:
         max_size = np.amax(array)
         bins = make_bins(max_size)
@@ -62,6 +67,9 @@ def make_distribution(array):
         print(f"Error {ve}: No empty arrays allowed")
     else:
         plt.hist(array, bins)
+        plt.savefig(f"..\\images\\{filename}.png")
+
+    if show:
         plt.show()
 
 
@@ -70,16 +78,23 @@ if __name__ == "__main__":
     content = read_file()
     print("Read file...")
 
-    print("Starting Algorithm")
+    print("Starting Algorithm for 0, 10 and 25% mismatch")
     case1 = mismatch(content, 0.0)
+    case2 = mismatch(content, 0.1)
+    case3 = mismatch(content, 0.25)
     print("Finished Algorithm")
 
-    print("Making distribution...")
-    make_distribution(case1)
+    print("Starting Algorithm for 0, 10 and 25% mismatch with ins/dels")
+    case1_modification = mismatch(content, 0.0, True)
+    case2_modification = mismatch(content, 0.1, True)
+    case3_modification = mismatch(content, 0.25, True)
+    print("Finished Algorithm")
 
-## TEST
-"""
-mismatch(0.0. )
-mismatch(0.10)
-mismatch(0.25)
-"""
+    print("Making distributions...")
+    make_distribution(case1, "distrib_length_0pc", show=False)
+    make_distribution(case2, "distrib_length_10pc", show=False)
+    make_distribution(case3, "distrib_length_25pc", show=False)
+
+    make_distribution(case1_modification, "distrib_length_0pc_mod", show=False)
+    make_distribution(case2_modification, "distrib_length_10pc_mod", show=False)
+    make_distribution(case3_modification, "distrib_length_25pc_mod", show=False)
