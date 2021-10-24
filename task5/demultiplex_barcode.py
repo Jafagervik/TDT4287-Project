2e5e8d2a80129d51a77310490d60d80c442493f9
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2021-10-24 15:28:46 +0200

    yea some updates or whatever

--- task5/demultiplex_barcode.py
+++ task5/demultiplex_barcode.py
@@ -21,19 +21,20 @@ from suffix_tree import Tree
 
 from helpers import read_file, A
 
-def make_suffix_tree(filename: str):
+
+def make_suffix_tree(lines: list[str]):
     """Creates a suffix tree of all sequences in file."""
-    lines = read_file(filename)
+
     seqs = {}
-    
+
     for idx, line in enumerate(lines):
         seqs[idx] = line
-    
-    tree =  Tree(seqs)   
+
+    tree = Tree(seqs)
     return tree
 
 
-def identify_barcode(tree: Tree) -> list[str]:
+def identify_barcodes(tree: Tree) -> list[str]:
     """Part 1 - identify the
     barcodes (3’ adapters) used and thereby how many samples were multiplexed"""
     # set up suffix tree
@@ -41,15 +42,14 @@ def identify_barcode(tree: Tree) -> list[str]:
     # Find common suffixes, and add these to list of barcodes
     barcodes = []
 
-
-
+    # TODO: set up traversing to find barcode
     # From the information of the task we know that the length is in range [4,8]
     if 4 <= len(barcode) <= 8:
-        barcode.append("yes")   
+        barcode.append("yes")
     return barcodes
 
 
-def num_of_seqs_per_sample(seqs: list[str])):
+def num_of_seqs_per_sample(seqs: list[str], barcodes: list[str]):
     """Part 2 - identify how many sequences that were sequenced from each sample."""
     # naive: return len(seqs)
     # actual: return nun of barcodes found in the tree
@@ -59,7 +59,7 @@ def num_of_seqs_per_sample(seqs: list[str])):
 def seq_length_dist():
     """Part 3 - identify
     the sequence length distribution within each sample."""
-    # Look through each 
+    # Look through each
     pass
 
 
@@ -76,5 +76,9 @@ def main():
 
 
 if __name__ == "__maim__":
-    tree = make_suffix_tree("../data/MultiplexedSamples.txt")
+    seqs = read_file("../data/MultiplexedSamples.txt")
+    tree = make_suffix_tree(seqs)
+    barcodes = identify_barcodes(tree)
+    seqs_per_sample = num_of_seqs_per_sample(seqs, barcodes)
+
     main()
