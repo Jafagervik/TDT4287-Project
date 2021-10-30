fc225aeead01c8078f013d0060c03eaf7b504f93
Jafagervik <jorgenafagervik@gmail.com>
2021-10-30 23:09:18 +0200

    feat: task 2 and 3

--- helpers.py
+++
@@ -1,6 +0,0 @@
-A = "TGGAATTCTCGGGTGCCAAGGAACTCCAGTCACACAGTGATCTCGTATGCCGTCTTCTGCTTG"
-
-
-def read_file(filename: str = ".\\data\\s_3_sequence_1M.txt"):
-    with open(filename, "r") as f:
-        return f.readlines()
--- task3/sequencing_error_dist.py
+++ task3/sequencing_error_dist.py
@@ -5,54 +5,79 @@ import numpy as np
 from numba import njit
 from tqdm import tqdm
 
-from helpers import read_file, A
+
+A = "TGGAATTCTCGGGTGCCAAGGAACTCCAGTCACACAGTGATCTCGTATGCCGTCTTCTGCTTG"
 
 
+def read_file(filename: str = "..\\data\\s_3_sequence_1M.txt"):
+    with open(filename, "r") as f:
+        return f.readlines()
+
+
+@njit(fastmath=True)
 def string_compare(
-    A: str, B: str, mismatch_percentage: float, allow_ins_del: bool = False
-):
+    a_prefix: str, B: str, allow_ins_del: bool = False
+) -> tuple[bool, bool]:
+    """
+    Hamming distance between two strings.
+
+    returns a tuple if it belongs to 0.10 or 0.25 mismatch array
+    """
     # TODO: implement insertion deletion
-    if A == B:
-        return True
+    if a_prefix == B:
+        return (True, True)
 
-    if not A or not B:
-        return False
-
-    max_len = max(len(A), len(B))
-    min_len = min(len(A), len(B))
+    if not a_prefix or not B:
+        return (False, False)
+    # Both strings have same value
+    l = len(a_prefix)
     same_char_at_index = 0
-    for i in range(min_len):
-        if A[i] == B[i]:
+    for i in range(l):
+        if a_prefix[i] == B[i]:
             same_char_at_index += 1
 
-    a = same_char_at_index / max_len
-    return a >= (1.0 - mismatch_percentage)
+    match = same_char_at_index / l
+    return (match >= (1.0 - 0.1), match >= (1.0 - 0.25))
 
 
-def mismatch(content, mismatch_percentage: float):
-    allowed_seqs = []
+def mismatch(seqs: list[str], allow_ins_del: bool = False) -> list[str]:
+    allowed_seqs_10 = []
+    allowed_seqs_25 = []
 
-    # FIXME: Handle exception better
-    for line in tqdm(content):
-        line = line.rstrip()
+    for line in tqdm(seqs):
+        line = line.rstrip()  # Remove trailing newline
         l = len(line)
+        suffix_10_found = False
+        suffix_25_found = False
         for i in range(l):
             # Match prefix of a with suffix of s or "line" in this instance
-            suffix_prefix = string_compare(A[: l - i], line[i:], mismatch_percentage)
-            print(suffix_prefix)
-            if suffix_prefix:
-                allowed_seqs.append(
-                    line
-                )  # If one suff / pre match we add the entire line
-                # We break since we're only interested in longest suffix
+            suffix_prefix_10, suffix_prefix_25 = string_compare(A[: l - i], line[i:])
+
+            if suffix_prefix_10 and not suffix_10_found:
+                allowed_seqs_10.append(
+                    # Add the suffix which is allowed, all sequences have same length anyways
+                    line[i:]
+                )
+                suffix_10_found = True
+
+            if suffix_prefix_25 and not suffix_25_found:
+                allowed_seqs_25.append(line[i:])
+
+                suffix_25_found = True
+
+            if suffix_10_found and suffix_25_found:
+                # We've now found suffix/prefix match for both 10 and 25%
+                # go to next sequence
                 break
 
-    return allowed_seqs
+    return np.array(allowed_seqs_10), np.array(allowed_seqs_25)
 
 
+@njit(fastmath=True)
 def seq_error_sequence(seqs: list[str]):
     """
-    @param seq: String sequence in S
+    Array of indexes where sequence error occurs
+    @param seqs: List of sequences
     """
     # At which indexes do we find these errors
     index_errors = []
@@ -68,65 +93,101 @@ def seq_error_sequence(seqs: list[str]):
     return np.array(index_errors)
 
 
-def seq_error_per_nucleotide(seqs: list[str], nucleotide: str):
+@njit(fastmath=True)
+def rate_of_seq_error(seqs: list[str]) -> float:
     """
     @param seqs: allowed sequences which passes even though there can be errors
-    @param nucleotide: A, C, T or G
     """
-    index_errors_per_nuc = []
+    sequence_error_count = 0
     for seq in seqs:
         l = len(seq)
+        # string sequence s and does not matchw with prefix of A
+        if A[:l] != seq:
+            sequence_error_count += 1
+            # Found error, go to next sequence
 
-        # Both strings are now of equal length
-        for i in range(l):
-            # The char in A we are looking at now need
-            if A[i] != seq[i] and A[i] == nucleotide:
-                index_errors_per_nuc.append(i)
+    return float(sequence_error_count / len(seqs))
 
-    return np.array(index_errors_per_nuc)
+
+@njit(fastmath=True)
+def make_bins(size: int):
+    return np.array([i for i in range(size + 1)])
+
+
+def make_distribution_task2(array, percentage: int, filename: str, show: bool = False):
+    assert len(array) > 0, "No empty arrays allowed"
+
+    array_lengths = np.vectorize(len)(array)
+
+    max_size = np.amax(len(max(array, key=len)))
+    bins = make_bins(max_size + 1)
+    plt.xlabel("Sequence lengts")
+    plt.ylabel("Frequency of lengths")
+    plt.title(
+        f"Histogram of sequence length from allowed sequcenes Mismatch allowed: {percentage}%"
+    )
+    plt.hist(array_lengths, bins)
+
+    plt.savefig(f"..\\images\\{filename}.png")
+
+    if show:
+        plt.show()
 
 
 def make_distribution(
-    seq_errors, nuc: str = None, has_nuc: bool = False, show: bool = False
+    seq_errors,
+    percentage: int,
+    show: bool = False,
 ):
+    plt.xlabel("Index in string")
+    plt.ylabel("Frequency of errors")
+
+    plt.title("Histogram of where error occurs for sequence")
     plt.hist(
         seq_errors, bins=np.arange(seq_errors.min(), seq_errors.max() + 1), align="left"
     )
-    plt.savefig(
-        f"..\\images\\sequence_error_rate_{nuc}.png"
-    ) if has_nuc else plt.savefig(f"..\\images\\sequence_error_rate.png")
+
+    plt.savefig(f"..\\images\\sequence_error_rate_mismatch{percentage}.png")
+
     if show:
         plt.show()
 
 
 if __name__ == "__main__":
+
     print("Reading file...")
     content = read_file()
     print("Read file...")
 
-    print("Starting Algorithm for sequence errors")
     # First of we allow 10% mismatch
-    mismatches = mismatch(content, 0.1)
-    # Make differne
-    index_errors = seq_error_sequence(mismatches)
-    index_errors_a = seq_error_per_nucleotide(mismatches, "A")
-    index_errors_t = seq_error_per_nucleotide(mismatches, "T")
-    index_errors_c = seq_error_per_nucleotide(mismatches, "C")
-    index_errors_g = seq_error_per_nucleotide(mismatches, "G")
-    print("Finished Algorithm...")
+    case10, case25 = mismatch(content)
 
-    print("Making distributions...")
-    make_distribution(index_errors, show=True)
-    make_distribution(index_errors_a, "A", has_nuc=True)
-    make_distribution(index_errors_t, "T", has_nuc=True)
-    make_distribution(index_errors_c, "C", has_nuc=True)
-    make_distribution(index_errors_g, "G", has_nuc=True)
+    print("Making distributions for task 2...")
+    make_distribution_task2(case10, 10, "distrib_length_10pc")
+    make_distribution_task2(case25, 25, "distrib_length_25pc")
+
+    print("Starting Task 3...")
+    index_errors_10 = seq_error_sequence(case10)
+    rate_seq_error_10 = rate_of_seq_error(case10)
+    print(
+        "Number of accepted sequences with 10% mismatch with errors:",
+        round(rate_seq_error_10, 2) * 100,
+        "percent",
+    )
+
+    print(r"Making distributions for 10% allowed mismatch...")
+    make_distribution(index_errors_10, 10)
+
+    # 25 PERCENT
+    index_errors_25 = seq_error_sequence(case25)
+    rate_seq_error_25 = rate_of_seq_error(case25)
+    print(
+        "Number of accepted sequences with 25% mismatch with errors:",
+        round(rate_seq_error_25, 2) * 100,
+        "percent",
+    )
+
+    print(r"Making distributions for 25% allowed mismatch...")
+    make_distribution(index_errors_25, 25)
+
     print("Finished making distributions...")
-
-
-## TEST
-"""
-mismatch(0.0. )
-mismatch(0.10)
-mismatch(0.25)
-"""
